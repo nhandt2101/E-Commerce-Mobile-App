1848cda2c64f82610518a09856d71066cac91bde
Đặng Trí Nhân <21020371@vnu.edu.vn>
2023-12-20 14:57:20 +0700

    new commit

--- src/screen/Cart/Shopping.jsx
+++ src/screen/Cart/Shopping.jsx
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, ScrollView } from "react-native";
+import { View, Text, ScrollView, FlatList, TextInput, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Navigator from "../../component/navigative";
-
+import Icon from "react-native-vector-icons/FontAwesome";
 import { getData, storeData } from "../../component/store";
-import { insertCart } from "../../db/cart";
+import { insertCart} from "../../db/cart";
 import { findProduct } from "../../db/product";
-
-
+import * as SQLite from 'expo-sqlite';
 export default function ShoppingCartScreen({ navigation }) {
+    const db = SQLite.openDatabase('Mobile.db');
     const [product, setProduct] = useState(null);
     const [products, setProducts] = useState(null);
     const [user, setUser] = useState(null);
     const [quantity, setQuantity] = useState(1);
     const [isAddingToCart, setIsAddingToCart] = useState(false);
+    const [comment, setComment] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
+  const [currentName, setCurrentName] = useState(undefined);
+    useEffect(() => {
+        db.transaction(tx => {
+          tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+        });
+    
+        db.transaction(tx => {
+          tx.executeSql('SELECT * FROM names', null,
+            (txObj, resultSet) => setComment(resultSet.rows._array),
+            (txObj, error) => console.log(error)
+          );
+        });
+        setIsLoading(false);
+  }, [db]);
 
     useEffect(() => {
         fetchData();
@@ -46,6 +63,20 @@ export default function ShoppingCartScreen({ navigation }) {
         }
     };
 
+    const addComment = () => {
+        db.transaction(tx => {
+          tx.executeSql('INSERT INTO names (name) values (?)', [currentName],
+            (txObj, resultSet) => {
+              let existingNames = [...comment];
+              existingNames.push({ id: resultSet.insertId, name: currentName});
+              setComment(existingNames);
+              setCurrentName(undefined);
+            },
+            (txObj, error) => console.log(error)
+          );
+        });
+      }
+
     const addToCart = async (item) => {
         try {
             const { describe, link_img, price, productName, sale_id } = item;
@@ -57,6 +88,17 @@ export default function ShoppingCartScreen({ navigation }) {
             console.log(e);
         }
     };
+
+    const showComments = () => {
+        return comment.map((comment, index) => {
+          return (
+            <View key={index} style={{flexDirection: "row", marginLeft:10}}>
+                <Icon name="user" size={30} color="blue"/>
+                <Text style={styles.fakeComment}> {comment.name}</Text>
+            </View>
+          );
+        });
+      };
 
     const renderItem = ({ item }) => (
         <TouchableOpacity style={styles.productItem} onPress={() => {
@@ -74,64 +116,93 @@ export default function ShoppingCartScreen({ navigation }) {
     );
 
     return (
-        <>
-            <ScrollView>
-                <View style={styles.container}>
-                    {product != null ? (
-                        <>
-                            <Text style={styles.title}>{product.productName}</Text>
-                            <View style={styles.content}>
-                                <View key={product.id} style={styles.productContainer}>
-                                    <Image source={{ uri: product.link_img }} style={styles.productImage} />
-                                    <Text style={styles.productPrice}>Price: {product.price}</Text>
-
-                                    <View style={styles.quantityContainer}>
-                                        <View style={styles.quantityButtonContainer}>
-                                            <TouchableOpacity
-                                                style={styles.quantityButton}
-                                                onPress={() => setQuantity(quantity - 1)}
-                                                disabled={quantity === 1}
-                                            >
-                                                <Text style={styles.quantityButtonText}>-</Text>
-                                            </TouchableOpacity>
-                                            <Text style={styles.quantityText}>{quantity}</Text>
-                                            <TouchableOpacity
-                                                style={styles.quantityButton}
-                                                onPress={() => setQuantity(quantity + 1)}
-                                            >
-                                                <Text style={styles.quantityButtonText}>+</Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                        <TouchableOpacity style={styles.buyButton}>
-                                            <Text style={styles.buyButtonText} onPress={() => addToCart(product)}>Add to Cart</Text>
+        <View style={styles.container}>
+            {product != null ? (
+                <>  
+                    <ScrollView style={{flex:1,}}>
+                        <View style={styles.content}>
+                            <View key={product.id} style={styles.productContainer}>
+                                <Image source={{ uri: product.link_img }} style={styles.productImage} />
+                            </View>
+                            <View style={{marginLeft: 5, marginRight:5,}}> 
+                                <Text>{product.productName}</Text>
+                                <Text style={{fontWeight:"bold", color:"red"}}>Price: {product.price}</Text>
+                                <View style={styles.quantityContainer}>
+                                    <View style={styles.quantityButtonContainer}>
+                                        <TouchableOpacity
+                                            style={styles.quantityButton}
+                                            onPress={() => setQuantity(quantity - 1)}
+                                            disabled={quantity === 1}
+                                        >
+                                            <Text style={styles.quantityButtonText}>-</Text>
+                                        </TouchableOpacity>
+                                        <Text style={styles.quantityText}>{quantity}</Text>
+                                        <TouchableOpacity
+                                            style={styles.quantityButton}
+                                            onPress={() => setQuantity(quantity + 1)}
+                                        >
+                                            <Text style={styles.quantityButtonText}>+</Text>
                                         </TouchableOpacity>
                                     </View>
+                                    <TouchableOpacity style={styles.buyButton}>
+                                        <Text style={styles.buyButtonText} onPress={() => addToCart(product)}>Add to Cart</Text>
+                                    </TouchableOpacity>
                                 </View>
+                                <Text>Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
                             </View>
+                        </View> 
+                        <View style={{flexDirection: "row", marginTop:20,}}>
+                            <TextInput
+                                value={currentName} 
+                                onChangeText={setCurrentName}
+                                style={styles.commentText}
+                                placeholder="  Danh gia san pham"
+                            />
+                            <TouchableOpacity onPress={addComment}>
+                                <Image source={require('../../../assets/send.png')} style={styles.commentButton}/>
+                            </TouchableOpacity>
+                        </View>
+                        {showComments()}
+                        <ScrollView max>
+                            <View style={{flexDirection: "row", marginLeft:10}}>
+                                <Icon name="user" size={30} color="blue"/>
+                                <Text style={styles.fakeComment}> Very nice</Text>
+                            </View>
+                            <View style={{flexDirection: "row", marginLeft:10}}>
+                                <Icon name="user" size={30} color="blue"/>
+                                <Text style={styles.fakeComment}> Oh my Goddddddddddddddd!!!!!!!!!!!!!!!!!</Text>
+                            </View>
+                            <View style={{flexDirection: "row", marginLeft:10}}>
+                                <Icon name="user" size={30} color="blue"/>
+                                <Text style={styles.fakeComment}> ạkfkjashfkjasncjaksvnajncasjb svjbbbbbbbbbbbbbjkakkkkkkkkkkádasdasa</Text>
+                            </View>
+                        </ScrollView>
 
-                            {products != null ? (
-                                <ScrollView>
-                                    <Text style={styles.sectionTitle}>Sản phẩm tương tự</Text>
-                                    <FlatList
-                                        data={products}
-                                        renderItem={renderItem}
-                                        keyExtractor={(item) => item.id}
-                                        numColumns={2}
-                                        contentContainerStyle={styles.productList}
-                                    />
-                                </ScrollView>
-                            ) : (
-                                <Text></Text>
-                            )}
+                        {products != null ? (
+                            <ScrollView>
+                                <Text style={styles.sectionTitle}>Sản phẩm tương tự</Text>
+                                <FlatList
+                                    data={products}
+                                    renderItem={renderItem}
+                                    keyExtractor={(item) => item.id}
+                                    numColumns={2}
+                                    contentContainerStyle={styles.productList}
+                                />
+                            </ScrollView>
+                        ) : (
+                            <Text></Text>
+                        )}
+                    </ScrollView>
+                    <Navigator/>
+                </>
+            ) : (
+                <>
+                    <Text>Loading...</Text>
+                    <Navigator />
+                </>
 
-                        </>
-                    ) : (
-                        <Text>Loading...</Text>
-                    )}
-                </View>
-            </ScrollView>
-            <Navigator />
-        </>
+            )}
+        </View>
     );
 };
 
@@ -148,11 +219,16 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        alignItems: "center",
     },
     productContainer: {
         alignItems: "center",
         marginBottom: 20,
+    },
+    productImage: {
+        width: 150,
+        height: 150,
+        resizeMode: "cover",
+        marginBottom: 5,
     },
     productName: {
         fontSize: 18,
@@ -166,6 +242,16 @@ const styles = StyleSheet.create({
         marginRight: 5,
         alignItems: "center",
     },
+    productItem: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 5,
+        padding: 10,
+        backgroundColor: "#f3f3f3",
+        borderRadius: 5,
+    },
     quantityContainer: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -175,6 +261,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginRight: 8,
+    },
+    sectionTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginTop: 20,
+        marginBottom: 10,
+        marginLeft: 10,
     },
     quantityButton: {
         backgroundColor: '#DDDDDD',
@@ -191,56 +284,49 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginHorizontal: 8,
     },
+    productDescription: {
+        fontSize: 12,
+        textAlign: "center",
+        marginBottom: 5,
+    },
     buyButton: {
-        backgroundColor: '#ee4d2d',
+        backgroundColor: 'blue',
         paddingHorizontal: 16,
         paddingVertical: 12,
         borderRadius: 4,
+    },
+    productList: {
+        paddingHorizontal: 10,
+    },
+    commentText: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        marginRight: 5,
+        marginLeft: 10,
+        borderRadius: 10,
+        flex : 1,
+    },
+    commentButton: {
+        height : 30,
+        width : 30,
+        marginTop : 5,
+        marginRight : 10,
     },
     buyButtonText: {
         color: 'white',
         fontSize: 16,
         fontWeight: 'bold',
     },
-    sectionTitle: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginTop: 20,
-        marginBottom: 10,
+    fakeComment: {
+        borderColor: "gray",
+        flex : 1,
+        minHeight: 30,
+        borderWidth : 1,
+        marginRight: 10,
+        marginBottom : 10, 
         marginLeft: 10,
-    },
-    productList: {
-        paddingHorizontal: 10,
-    },
-    productItem: {
-        flex: 1,
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 5,
-        padding: 10,
-        backgroundColor: "#f3f3f3",
-        borderRadius: 5,
-    },
-    productImage: {
-        width: 150,
-        height: 150,
-        resizeMode: "cover",
-        marginBottom: 5,
-    },
-    productName: {
-        fontSize: 16,
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    productDescription: {
-        fontSize: 12,
-        textAlign: "center",
-        marginBottom: 5,
-    },
-    productPrice: {
-        fontSize: 14,
-        fontWeight: "bold",
-        color: "#ee4d2d",
+        borderRadius: 10,
     },
 });
